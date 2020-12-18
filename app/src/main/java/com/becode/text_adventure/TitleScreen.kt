8d3186718350feb238623033e1fefb8af1eee9a4
Beardificent <cis.magito@gmail.com>
2020-12-18 12:19:37 +0100

    added a listener to the titleButton to start the game

--- app/src/main/java/com/becode/text_adventure/TitleScreen.kt
+++ app/src/main/java/com/becode/text_adventure/TitleScreen.kt
@@ -1,11 +1,19 @@
 package com.becode.text_adventure
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_title_screen.*
 
 class TitleScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title_screen)
+        //this calls on the button on the titlescreen
+        titleButton.setOnClickListener{
+            //this declares the intent of what it's going to do after listening.
+            val Intent = Intent(this, GameScreen:: class.java)
+            startActivity(Intent)
+        }
     }
 }
